8fe1ee891dc9f1e5d8b76b0d0ceb4dc4bd396ef5
Jose Castro <joseocastroarias@gmail.com>
2021-03-30 22:22:59 -0500

    Refactor tests code.

--- Gemfile
+++ Gemfile
@@ -34,7 +34,6 @@ group :test do
   gem 'factory_bot'
   gem 'rspec-rails', '~> 4.1.0'
   gem 'simplecov', require: false
-  gem 'rails-controller-testing'
 end
 
 group :development, :test do
--- spec/controllers/api/v1/tasks_controller_spec.rb
+++ spec/controllers/api/v1/tasks_controller_spec.rb
@@ -1,23 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::TasksController, type: :controller do
-  let(:tasks) { create_list(:task, 10) }
-
   # index action
   describe 'GET index' do
     subject { get :index, format: :json }
 
-    it 'status should be' do
-      expect(subject.status).to eq(200)
+    it { is_expected.to have_http_status(:ok) }
+
+    context 'when there are not tasks' do
+      it { expect(subject.body).to eq('[]') }
     end
 
-    it 'when there are not tasks' do
-      expect(subject.body).to eq('[]')
-    end
+    context 'when there are tasks' do
+      let!(:tasks) { create_list(:task, 10) }
 
-    it 'when there are tasks' do
-      tasks
-      expect(subject.body).to eq(Task.all.to_json)
+      it { expect(subject.body).to eq(Task.all.to_json) }
     end
   end
 
@@ -26,7 +23,16 @@ RSpec.describe Api::V1::TasksController, type: :controller do
     subject { post :create, format: :json, params: params }
 
     context 'when params are correct' do
-      let(:params) { { task: { title: Faker::Hacker.say_something_smart, description: Faker::Hacker.adjective, time: Faker::Number.number(digits: 3) } } }
+      let(:params) do
+        {
+          task:
+            {
+              title: Faker::Hacker.say_something_smart,
+              description: Faker::Hacker.adjective,
+              time: Faker::Number.number(digits: 3)
+            }
+        }
+      end
 
       it 'status should be' do
         expect(subject.status).to eq(201)
@@ -47,9 +53,7 @@ RSpec.describe Api::V1::TasksController, type: :controller do
       let(:params) { { id: task.id, task: { title: 'New name' } } }
 
       context 'when data is provided is valid' do
-        it 'status should be' do
-          expect(subject.status).to eq(200)
-        end
+        it { is_expected.to have_http_status(:ok) }
 
         it 'is expected to update user' do
           expect(subject.body).to include('New name')
